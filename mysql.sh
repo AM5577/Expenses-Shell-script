cda23ca7a31920f4a409c0eec094f3decf59698d
Arun M <arun.maddukuri07@gmail.com>
2025-10-28 11:28:17 +0530

    updated mysql script

--- mysql.sh
+++ mysql.sh
@@ -44,7 +44,7 @@ VALIDATE $? "Installing MYSQL server"
 systemctl enable mysqld
 VALIDATE $? "Enabling MYSQL service" &>>$LOG_FILE_NAME
 
-systemctl start mysqld $>>$LOG_FILE_NAME
+systemctl start mysqld &>>$LOG_FILE_NAME
 VALIDATE $? "Starting mysql server"
 
 mysql_secure_installation --set-root-pass ExpenseApp@1
